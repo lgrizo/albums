ad1b484ebe302ee544c63bb3d253672991d425ab
Luis Rizo <rizo.luis@yahoo.com>
2017-07-23 19:51:44 -0400

    first commit

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+//App.js - place code below for iOS!!
+
+// Import a library to help create a Component
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
